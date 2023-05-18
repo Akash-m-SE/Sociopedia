87d76d35949b9de8900614fa1757c0b7b581401b
Akash-m-SE <72149737+Akash-m-SE@users.noreply.github.com>
2023-05-18 19:05:02 +0530

    first commit

---
+++ server/controllers/posts.js
@@ -0,0 +1,77 @@
+import Post from "../models/Post.js";
+import User from "../models/User.js";
+
+// CREATE
+export const createPost = async (req, res) => {
+  try {
+    const { userId, description, picturePath } = req.body;
+    const user = await User.findById(userId);
+    const newPost = new Post({
+      userId,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      location: user.location,
+      description,
+      userPicturePath: user.picturePath,
+      picturePath,
+      likes: {},
+      comments: [],
+    });
+    await newPost.save();
+
+    const post = await Post.find();
+
+    res.status(201).json(post);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
+
+// READ
+export const getFeedPosts = async (req, res) => {
+  try {
+    const post = await Post.find();
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getUserPosts = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const post = await Post.find({ userId });
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+// UPDATE
+export const likePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.body;
+    const post = await Post.findById(id);
+    const isLiked = post.likes.get(userId);
+
+    // like is like a dictionary with userid as key and true as value. We are checking if the userId exists in like dict or not
+    // if the userId exists then our isLiked will be true indicating it exists, so we simply delete the userId from our likes dict else we
+    // add the userId to our likes dict and set its val to be true indicating that the user has liked the post
+    if (isLiked) {
+      post.likes.delete(userId);
+    } else {
+      post.likes.set(userId, true);
+    }
+
+    const updatedPost = await Post.findByIdAndUpdate(
+      id,
+      { likes: post.likes },
+      { new: true }
+    );
+
+    res.status(200).json(updatedPost);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
---
+++ server/routes/posts.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+// READ
+router.get("/", verifyToken, getFeedPosts); //gets every single post from the database while we are in the home page
+router.get("/:userId/posts", verifyToken, getUserPosts); //gets only the posts from the user while we are in his home page
+
+// UPDATE
+router.patch("/:id/like", verifyToken, likePost); //for liking and unliking the post
+
+export default router;
